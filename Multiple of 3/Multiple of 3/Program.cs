81fdd8f67f96b4d645fe9b1ec4c543d592b84f86
Abdullah Alsalem <abdullahsalem18@gmail.com>
2021-05-04 09:32:46 +0300

    Done

---
+++ Multiple of 3/Multiple of 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Multiple_of_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //your code goes here
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("*");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
